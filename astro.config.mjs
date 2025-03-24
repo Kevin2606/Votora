80f271833a406872eb9d453d9375a09cbcd67e65
Kevin Gallardo <andreskevin2606@gmail.com>
2025-03-24 01:53:07 -0500

    feat: Actualizar la configuración de la base en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kevin2606.github.io',
-  base: '.',
 
   vite: {
     plugins: [tailwindcss()],
